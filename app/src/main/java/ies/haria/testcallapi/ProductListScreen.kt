fc58c94f9c43083073d2280af217ac5cbe87cf3e
fmoorenoo <fermontemore@gmail.com>
2025-01-24 09:14:07 +0000

    ProductListScreen y SearchScreen terminados

--- app/src/main/java/ies/haria/testcallapi/ProductListScreen.kt
+++ app/src/main/java/ies/haria/testcallapi/ProductListScreen.kt
@@ -2,130 +2,40 @@ package ies.haria.testcallapi
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import ies.haria.testcallapi.network.product.model.ProductResponse
+import ies.haria.testcallapi.data.ProductDBViewModel
+import ies.haria.testcallapi.data.ProductViewModel
+import ies.haria.testcallapi.data.db.productResponseToProduct
+import ies.haria.testcallapi.network.ProductResponse
 
 @Composable
 fun ProductListScreen(
     productViewModel: ProductViewModel,
-    context: Context,
-    innerPaddingValues: PaddingValues
+    databaseViewModel: ProductDBViewModel,
+    context: Context
 ) {
     val isLoading: Boolean by productViewModel.isLoading.observeAsState(initial = true)
     if (isLoading) {
         productViewModel.getAllProducts()
         LoadingScreen()
     } else {
-        CompleteProductListScreen(productViewModel.productList.value!!, innerPaddingValues, context)
+        ProductListView("Listado de productos", productViewModel.productList.value!!, databaseViewModel, context)
     }
 }
 
 @Composable
-fun CompleteProductListScreen(
+fun ProductListView(
+    title: String,
     productList: List<ProductResponse>,
-    innerPaddingValues: PaddingValues,
+    databaseViewModel: ProductDBViewModel,
     context: Context
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        item {
-            Text(
-                modifier = Modifier.padding(top = 25.dp, bottom = 10.dp),
-                text = "Lista de productos",
-                fontSize = TextUnit(8f, TextUnitType.Em),
-                fontWeight = FontWeight.Bold
-            )
-        }
-        if (productList.isEmpty()) {
-            item {
-                Text(
-                    modifier = Modifier.padding(top = 25.dp),
-                    text = "Lista vacía",
-                    fontSize = TextUnit(8f, TextUnitType.Em)
-                )
-            }
-        } else {
-            items(productList) { product ->
-                Card(
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.surfaceVariant
-                    ),
-                    modifier = Modifier.width(270.dp).padding(bottom = 25.dp)
-                ) {
-                    Row(
-                        modifier = Modifier.padding(10.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = product.title,
-                            fontSize = TextUnit(5.5f, TextUnitType.Em),
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.weight(1f).padding(end = 10.dp)
-                        )
-                        FilledIconButton(
-                            modifier = Modifier.size(20.dp),
-                            onClick = {
-                                Toast.makeText(context, "Producto añadido", Toast.LENGTH_SHORT).show()
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Filled.Add,
-                                contentDescription = "Añadir elemento"
-                            )
-                        }
-                    }
-                    HorizontalDivider(
-                        modifier = Modifier.padding(bottom = 10.dp),
-                        thickness = 1.dp,
-                        color = Color.Gray
-                    )
-                    Column (
-                        modifier = Modifier.fillMaxSize().padding(start = 5.dp, bottom = 10.dp),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        AsyncImage(
-                            modifier = Modifier.size(160.dp),
-                            model = product.thumbnail,
-                            contentDescription = "Imagen de ${product.title}"
-                        )
-                    }
-                }
-            }
-        }
+    ProductList(title, productList, Icons.Filled.Add) { product: ProductResponse ->
+        Toast.makeText(context, "Producto añadido", Toast.LENGTH_SHORT).show()
+        databaseViewModel.insertOrUpdateFavoriteProduct(productResponseToProduct(product))
     }
 }
--- app/src/main/java/ies/haria/testcallapi/SearchScreen.kt
+++ app/src/main/java/ies/haria/testcallapi/SearchScreen.kt
@@ -1,12 +1,60 @@
 package ies.haria.testcallapi
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
+import android.content.Context
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ies.haria.testcallapi.data.ProductDBViewModel
+import ies.haria.testcallapi.data.ProductViewModel
 
 @Composable
-fun SearchScreen() {
-    Box() {
-        Text("Pantalla de búsqueda")
+fun SearchScreen(
+    productViewModel: ProductViewModel,
+    databaseViewModel: ProductDBViewModel,
+    context: Context
+) {
+    val isLoading: Boolean by productViewModel.isLoading.observeAsState(initial = false)
+    var searchString by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(top = 10.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = searchString,
+                label = { Text("Buscar producto") },
+                onValueChange = { searchString = it },
+                trailingIcon = { Icon(Icons.Filled.Search, contentDescription = "Icono búsqueda") }
+            )
+            Button(
+                onClick = { productViewModel.searchProducts(searchString) },
+                modifier = Modifier.padding(start = 5.dp)
+            ) {
+                Text("Buscar")
+            }
+        }
+        if (isLoading) {
+            LoadingScreen()
+        } else {
+            ProductListView(
+                "Productos encontrados",
+                productViewModel.productSearchList.value!!,
+                databaseViewModel,
+                context
+            )
+        }
     }
 }
